c8954eb69d6b4e8674756677dd4565d91bb8752a
drews16 <ggg.andreygnatyuk63@gmail.com>
2023-01-17 17:13:45 +0400

    Add Cart.cshtml

---
+++ ItVisShop/Controllers/CartController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ItVisShop.Controllers
+{
+    public class CartController : Controller
+    {
+        [HttpGet]
+        public IActionResult Cart()
+        {
+            return View();
+        }
+    }
+}
